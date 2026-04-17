76141e6df717fc61133107426ef7cd6abca922b0
zjw <2050344307@qq.com>
2026-04-17 19:58:29 +0800

    2026.4.17 项目创建

---
+++ src/test/java/com/omnisource/OmniSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnisource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmniSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
